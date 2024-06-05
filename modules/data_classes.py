a96093fbbe0ca472ad21ce166b19cfd6e53b9b35
DmitriiK <dklmn@mail.ru>
2024-06-05 14:33:57 +0300

    serialization of data classed to yaml without sorting

--- modules/data_classes.py
+++ modules/data_classes.py
@@ -2,6 +2,7 @@ from enum import Enum
 from typing import List, Optional
 
 from langchain_core.pydantic_v1 import BaseModel, Field
+import yaml
 
 
 class DCS_Type(str, Enum):
@@ -22,7 +23,19 @@ class DCS(BaseModel):
     target_table: str = Field(description='Target table for data-changing SQL statement')
     source_tables: Optional[List[str]] = Field(description='Names of source tables or views for SQL statement')
 
+    def to_dict(self):
+        # Create a dictionary representation of the instance with conditional logic
+        data = {'crud_type': self.crud_type.value, 'target_table': self.target_table}
+        if self.source_tables:
+            data['source_tables'] = self.source_tables
+        return data
+
 
 class SP_DCSs(BaseModel):
     sp_name: str = Field(description='Stored procedure name')
     DCSs: List[DCS] = Field()
+
+    def to_yaml(self):
+        # Convert nested DCS objects to dictionaries
+        data = {'sp_name': self.sp_name, 'DCSs': [dcs.to_dict() for dcs in self.DCSs]}
+        return yaml.dump(data, sort_keys=False)
--- modules/pipeline.py
+++ modules/pipeline.py
@@ -3,30 +3,31 @@ import os
 
 import modules.llm_communicator as llmc
 from modules.data_classes import SP_DCSs, DCS, DCS_Type
-from pydantic_yaml import to_yaml_str
-
-model = llmc.LLMCommunicator()
+import logging
 
 
 def export_to_yaml(inst: SP_DCSs, dir: str = None, file_name: str = None):
-    test_yml_str = to_yaml_str(inst)
+    test_yml_str = inst.to_yaml()
+    print(test_yml_str)
     file_name = file_name or f'{inst.sp_name}.yaml'
     if dir:
         file_name = os.path.join(dir, file_name)
+    logging.info(f'saving to {file_name}')
     with open(file_name, 'w') as yaml_file:
         yaml_file.write(test_yml_str)
 
 
 def take_analyze_and_save():
     sql_script_path = r'D:\projects\DataFeedEngine\DataFeedEngineIndex\stg\Stored procedures\PullData_RussellUS2_IndexSecurityValue_prc.sql'
-
+    model = llmc.LLMCommunicator()
     with open(sql_script_path) as f:
         script = f.read()
     out = model.request_and_parse(script)
     export_to_yaml(out, dir=r'.\data\output')
-    
-    
+
+
 def test_yaml():
+    print('test yaml')
     sp_name = 'SP_CRUDs_example'
     inst = SP_DCSs(sp_name=sp_name, DCSs=[DCS(target_table='[stg].[targ1]', crud_type=DCS_Type.TRUNCATE),
                                           DCS(target_table='[stg].[targ1]', crud_type=DCS_Type.INSERT,
