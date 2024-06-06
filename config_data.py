2f2f1c93b3f4af23a0417ddb052551de3f90028e
DmitriiK <dklmn@mail.ru>
2024-06-06 13:03:51 +0300

    enum for chat gpt models

--- config_data.py
+++ config_data.py
@@ -1,12 +1,23 @@
 import os
 from dotenv import load_dotenv
 import logging
+from enum import Enum
+
+
+class GPT_Model(str, Enum):
+    GPT_4 = 'gpt-4'
+    GPT_3_5_Turbo = 'gpt-3.5-turbo'
+    GPT_4o = 'gpt-4o'
+
+    def __repr__(self) -> str:
+        return str.__repr__(self.value)
+
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
 OPENAI_API_KEY = os.getenv('OPEN_AI_TOKEN')
-LLM_MODEL_NAME = 'gpt-4'  # gpt-3.5-turbo
+LLM_MODEL_NAME = GPT_Model.GPT_4o
 
 PARSE_SP_PROMPT_PATH = r'prompt\parse_sp_prompt.txt'
 SP_EXAMPLE_PATH = r'prompt\examples\sp_example.sql'
