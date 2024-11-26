f25deba05865ef8ef5db7b53686ac45790956387
DmitriiK <dklmn@mail.ru>
2024-11-26 14:38:21 +0300

    fixed path error

--- tests/test_llm.py
+++ tests/test_llm.py
@@ -5,6 +5,6 @@ from modules.pipeline import analyze_file_by_llm
 
 class TestLLM(unittest.TestCase):
     def test_parse_sp(self):
-        file_name = Path(r"data/output/input/datafeedOut_generateChangeFilesAddress_prc.sql" )
+        file_name = Path(r"data/input/datafeedOut_generateChangeFilesAddress_prc.sql" )
         analyze_file_by_llm(file_name)
 
