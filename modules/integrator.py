b52b7e8b3c7625e6a394de1eed2ae97e42b08660
dmitrii.kalmanovich <dmitrii_kalmanovich@AVVDID733>
2024-12-03 16:52:55 +0000

    building of dependincies chain

--- modules/integrator.py
+++ modules/integrator.py
@@ -11,7 +11,7 @@ sx.close_connection_finally = False
 llm = LLMCommunicator()
 
 
-def build_data_flow_graph(table_name: str, ret_chain: List[SP_DCSs] = None) -> List[SP_DCSs]:
+def build_data_flow_graph(table_name: str) -> List[SP_DCSs]:
     """To collect all stored procedures and tables, that participate as upstream source of data flow for some table
 
     Args:
@@ -21,28 +21,35 @@ def build_data_flow_graph(table_name: str, ret_chain: List[SP_DCSs] = None) -> L
     Returns:
         List[SP_DCSs]: _description_
     """
-    if not ret_chain:
-        ret_chain = []
-    depending = sx.get_depending(table_name)
-    for d in depending:
-        if d.type in {DB_Object_Type.SQL_STORED_PROCEDURE}:
-            #  TODO - add filtering by obj type on server level
-            defn = sx.get_module_def(d.full_name)
-            sp = llm.request_and_parse(defn)
-            data_inp_stms = [stm for stm in sp.DCSs  
+    ret_chain, target_tables = [], []
+
+    def traverse_dependencies(table_name: str):
+        target_tables.append(table_name) # need to track tables already passed to on prev leevels to avoid eternal loop
+        depending_sps = sx.get_depending(table_name, DB_Object_Type.SQL_STORED_PROCEDURE)
+        for dsp in depending_sps:
+            parsed_before = [x for x in ret_chain if sql_objs_are_eq(x.sp_name, dsp.name)]  # TODO compare by full name
+            # we can already have necessary instanse for this sp in prev iteration
+            if parsed_before:
+                sp_stms = parsed_before[0]
+            else:
+                defn = sx.get_module_def(dsp.full_name)
+                sp_stms = llm.request_and_parse(defn)
+            data_inp_stms = [stm for stm in sp_stms.DCSs
                              if sql_objs_are_eq(stm.target_table, table_name)
                              and stm.source_tables]
-            # if among SQL statments we have ones with our table as data flow target
+            # filtered out the statements where our table is target for data read
             if data_inp_stms:
-                ret_chain.append(sp)
+                ret_chain.append(sp_stms)
                 for stm in data_inp_stms:  # TODO consider data flow chains inside SP
-                    src_tbls = [t for t in stm.source_tables 
+                    src_tbls = [t for t in stm.source_tables
                                 if t.endswith('_tbl')
-                                and not sql_objs_are_eq(t, table_name)]
+                                and not any(sql_objs_are_eq(t, tt) for tt in target_tables)]
                     # TODO ret rid on relying on naming conv
                     for vw in (t for t in stm.source_tables if t.endswith('_vw')):
                         vw_tbls = sx.get_dependent(vw) # TODO consider recursive relations in views
                         src_tbls.extend(vw_tbls)
                     for src_tbl in src_tbls:
-                        build_data_flow_graph(src_tbl, ret_chain)
+                        traverse_dependencies(src_tbl)
+
+    traverse_dependencies(table_name=table_name)
     return ret_chain
--- tests/test_integrator.py
+++ tests/test_integrator.py
@@ -1,8 +1,9 @@
 import unittest
 from modules.integrator import build_data_flow_graph
 
+
 class TestIntegrator(unittest.TestCase):
 
     def test_build_data_flow_graph(self):
         ch = build_data_flow_graph('RussellUS_Constituent_tbl')
-        assert ch
+        assert len(ch) > 1
