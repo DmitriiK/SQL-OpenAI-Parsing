d4db46861217c85e23a26828c186201df1556708
dmitrii.kalmanovich <dmitrii_kalmanovich@AVVDID733>
2024-12-07 16:33:35 +0000

    format of file name

--- modules/pipeline.py
+++ modules/pipeline.py
@@ -8,13 +8,14 @@ from config_data import INPUT_PATH_SPs, INPUT_PATH_BASE_DIR, OUTPUT_FILE_EXTENSI
 import modules.llm_communicator as llmc
 from modules.view_parser import extract_table_view_names
 from modules.data_classes import SP_DCSs
+from modules.sql_modules.sql_string_helper import shorten_full_name
 
 
 def export_to_yaml(inst: SP_DCSs, dir: str = None, file_name: str = None):
     test_yml_str = inst.to_yaml()
     if dir and not os.path.exists(dir):
         os.makedirs(dir)
-    file_name = file_name or f'{inst.sp_name}.{OUTPUT_FILE_EXTENSION}'
+    file_name = file_name or f'{shorten_full_name(inst.sp_name)}.{OUTPUT_FILE_EXTENSION}'
     file_path = Path(dir, file_name) if dir else Path(file_name)
     logging.info(f'saving to {file_path}')
     with open(file_path, 'w') as yaml_file:
