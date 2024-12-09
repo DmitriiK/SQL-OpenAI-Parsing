13b31e1d66f5aa6189a9ed3a7be7c75cd592d18b
dmitrii.kalmanovich <dmitrii_kalmanovich@AVVDID733>
2024-12-09 15:50:00 +0000

    big refactoring

--- modules/integrator.py
+++ modules/integrator.py
@@ -1,9 +1,9 @@
 
-from typing import Iterable
+from typing import Iterable, List, Set
 from collections import defaultdict
 from joblib import Memory
 
-from modules.data_classes import DB_Object_Type, SP_DCSs
+from modules.data_classes import DB_Object_Type, SP_DCSs, DCS
 from .sql_modules.sql_engine import SQL_Executor
 from .llm_communicator import LLMCommunicator
 from .sql_modules.sql_string_helper import sql_objs_are_eq, get_table_schema_db, shorten_full_name
@@ -24,26 +24,49 @@ def request_and_parse(sql_script: str) -> SP_DCSs:
     return llm.request_and_parse(sql_script)
 
 
+def sp_to_mmd(trt_tbl: str, sp_name,  data_inp_stms: List[DCS]) -> Set[str]:
+    trg_node_id = mmd.add_node(node_caption=shorten_full_name(trt_tbl), id_is_caption=False)
+    src_tbls = []
+    tbl2stms = defaultdict(list) 
+    # to consider cases when we have several statements for one pair of source-table target table inside SP             
+    for stm in data_inp_stms:  # TODO consider data flow chains inside SP
+        src_tbls.extend([t for t in stm.source_tables if t.endswith('_tbl') and t not in src_tbls])
+        # TODO ret rid on relying on naming conv
+        for vw in (t for t in stm.source_tables if t.endswith('_vw')):
+            vw_tbls = sx.get_dependent(vw)  # TODO consider recursive relations in views
+            src_tbls.extend([vw_tbl.full_name for vw_tbl in vw_tbls if vw_tbl.full_name not in src_tbls])
+        for src_tbl in src_tbls:
+            tbl2stms[src_tbl].append(stm)
+    #  taking source tables from all statmemtns inside SP with aggregation by source table and adding bunch of nodes 
+    for src_tbl, stms in tbl2stms.items():
+        nc = shorten_full_name(src_tbl)  # new node caption
+        stms_sum = ','.join([str(stm.crud_type)[0:3] for stm in stms])
+        ec = f'{shorten_full_name(sp_name)}: {stms_sum}'  # new edge caption
+        mmd.add_source_node(node_caption=nc, node_id=trg_node_id, edge_caption=ec)
+        print(f'{src_tbl}-->{trt_tbl}')
+    return tbl2stms.keys()
+
+
 def build_data_flow_graph(table_name: str) -> Iterable[SP_DCSs]:
     """To collect all stored procedures and tables, 
-    that participate as upstream source of data flow for some table.
-    taking list of SPs, 
-    finding target table as target table in the SP list  of the statements,
-    for each of the statements take source tables
-    for each of them find SPs, where they are targeted
-    if no new SP for SP have been find, recursion stops,  - we are at the top level of data stream
+        that participate as upstream source of data flow for some table.
+        taking list of SPs, 
+        finding target table as target table in the SP list  of the statements,
+        for each of the statements take source tables
+        for each of them find SPs, where they are targeted
+        if no new SP for SP have been find, recursion stops,  - we are at the top level of data stream
 
-    Args:
-        table_name (str): _description_
-        ret_chain (List[SP_DCSs], optional): _description_. Defaults to None.
+        Args:
+            table_name (str): _description_
+            ret_chain (List[SP_DCSs], optional): _description_. Defaults to None.
 
-    Returns:
-        List[SP_DCSs]: _description_
+        Returns:
+            List[SP_DCSs]: _description_
     """
     ret_chain, target_tables = [], []
 
     def traverse_dependencies(table_name: str):
-        target_tables.append(table_name) # need to track tables already passed to on prev leevels to avoid eternal loop
+        target_tables.append(table_name) # need to track tables already passed to on prev levels to avoid eternal loop
         tsd = get_table_schema_db(table_name)
         if tsd[2] and tsd[2].lower() == 'indexdata':
             return  # TODO get rid of ugly shit
@@ -56,47 +79,37 @@ def build_data_flow_graph(table_name: str) -> Iterable[SP_DCSs]:
             else:
                 defn = sx.get_module_def(dsp.full_name)
                 sp_stms = request_and_parse(defn)
-            data_inp_stms = [stm for stm in sp_stms.DCSs
-                             if sql_objs_are_eq(stm.target_table, table_name)
-                             and stm.source_tables]
-            # filtered out the statements where our table is target for data read
-            if data_inp_stms:  # if we have at least one statement where our table is target
-                ret_chain.append(sp_stms)
-                yield sp_stms
-                # TODO - separate
-                trg_node_id = mmd.add_node(node_caption=shorten_full_name(table_name), id_is_caption=False)
-                src_tbls = []
-                tbl2stms = defaultdict(list) 
-                # to consider cases when we have several statements for one pair of source-table target table inside SP             
-                for stm in data_inp_stms:  # TODO consider data flow chains inside SP
-                    src_tbls.extend([t for t in stm.source_tables if t.endswith('_tbl') and t not in src_tbls])
-                    # TODO ret rid on relying on naming conv
-                    for vw in (t for t in stm.source_tables if t.endswith('_vw')):
-                        vw_tbls = sx.get_dependent(vw)  # TODO consider recursive relations in views
-                        src_tbls.extend([vw_tbl.full_name for vw_tbl in vw_tbls if vw_tbl.full_name not in src_tbls])
-                    for src_tbl in src_tbls:
-                        tbl2stms[src_tbl].append(stm)
-                #  taking source tables from all statmemtns inside SP with aggregation by source table and adding bunch of nodes 
-                for src_tbl, stms in tbl2stms.items():
-                    nc = shorten_full_name(src_tbl)  # new node caption
-                    stms_sum = ','.join([str(stm.crud_type)[0:3] for stm in stms])
-                    ec = f'{shorten_full_name(sp_stms.sp_name)}: {stms_sum}'  # new edge caption
-                    mmd.add_source_node(node_caption=nc, node_id=trg_node_id, edge_caption=ec)
-                    print(f'{src_tbl}-->{table_name}')
-
-                for src_tbl in tbl2stms:
-                    if not any(sql_objs_are_eq(src_tbl, tt) for tt in target_tables):
-                        yield from traverse_dependencies(src_tbl)
+                parsed_before.append(sp_stms)
+                data_inp_stms = [stm for stm in sp_stms.DCSs
+                                 if sql_objs_are_eq(stm.target_table, table_name)
+                                 and stm.source_tables]
+                yield sp_stms, data_inp_stms, table_name
+                srs_tbls = set(src_tbl for stm in data_inp_stms for src_tbl in stm.source_tables
+                               if not any(sql_objs_are_eq(src_tbl, tt) for tt in target_tables))
+                for src_tbl in srs_tbls:
+                    yield from traverse_dependencies(src_tbl)
 
     yield from traverse_dependencies(table_name=table_name)
-    mmd_out = mmd.generate_mermaid_code()
-    print(mmd_out)
 
 
 def data_flow_to_yaml(table_name: str, output_folder_path: str) -> int:
     sps = build_data_flow_graph(table_name=table_name)
     cnt = 0
-    for sp in sps:
-        export_to_yaml(sp, output_folder_path)
-        cnt += 1
+    for sp, inp_stms, _ in sps:
+        if inp_stms:
+            export_to_yaml(sp, output_folder_path)
+            cnt += 1
     return cnt
+
+
+def data_flow_to_mermaid(table_name: str) -> int:
+    sps = build_data_flow_graph(table_name=table_name)
+    cnt = 0
+    for sp, inp_stms, trg_tbl in sps:
+        if inp_stms:
+            sp_to_mmd(sp_name=sp.sp_name, trt_tbl=trg_tbl, data_inp_stms=inp_stms)
+            cnt += 1
+    mmd_out = mmd.generate_mermaid_code()
+    return mmd_out
+
+
--- tests/test_integrator.py
+++ tests/test_integrator.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 
-from modules.integrator import build_data_flow_graph, data_flow_to_yaml
+from modules.integrator import data_flow_to_yaml, data_flow_to_mermaid
 from config_data import OUTPUT_PATH
 
 trg_tbl = 'RussellUS_Constituent_tbl'
@@ -9,8 +9,13 @@ trg_tbl = 'RussellUS_Constituent_tbl'
 
 class TestIntegrator(unittest.TestCase):
         
-    def test_build_data_flow_graph(self):
+    def test_data_flow_to_yaml(self):
         out_dir_pth = os.path.join(OUTPUT_PATH, f'target_table = {trg_tbl}')
         cnt = data_flow_to_yaml(trg_tbl, out_dir_pth)
         assert cnt > 3
+
+    def test_data_flow_to_mermaid(self):
+        cnt = data_flow_to_mermaid(trg_tbl)
+        assert cnt > 3
+    
        
